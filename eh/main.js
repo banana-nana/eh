f9f056c2ff5d7d19d151ddabf538e91c1633631c
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-04-20 11:10:50 -0500

    Update main.js

--- eh/main.js
+++ eh/main.js
@@ -2,7 +2,6 @@ alert("Injecting Hacks");
       var h = prompt("Blooket or Quizizz hacks", "Quizizz");
 if (h === "Quizizz") {
   var x = document.createElement("script");
-        x.src = "https://cdn.jsdelivr.net/gh/gbaranski/quizizz-cheat@master/dist/bundle.js";);
+        x.src = "https://cdn.jsdelivr.net/gh/gbaranski/quizizz-cheat@master/dist/bundle.js"
         document.head.appendChild(x);
 }
-
