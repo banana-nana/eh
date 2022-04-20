2e8fd7493284b93603a0ea02b5cb6ef50b30eccf
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-04-20 11:00:15 -0500

    Update login.js

--- eh/login.js
+++ eh/login.js
@@ -6,7 +6,7 @@ if (user === "corp") {
     if (pass === "banana") {
         alert("Welcome back, " + user);
         var x = document.createElement("script");
-        x.src = "https://cdn.jsdelivr.net/gh/banana-nana/eh@master/eh/main.js";);
+        x.src = "https://cdn.jsdelivr.net/gh/banana-nana/eh@master/eh/main.js";
         document.head.appendChild(x);
     }else {
     alert("Wrong.")
