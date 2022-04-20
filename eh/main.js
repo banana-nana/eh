06fbf9dec1c27b8216a35268949dec9deab48bba
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-04-20 10:38:43 -0500

    quizlets

--- eh/main.js
+++ eh/main.js
@@ -1,1 +1,7 @@
- 
+alert("Injecting Hacks);
+      var h = prompt("Blooket or Quizizz hacks", "Quizizz");
+if (h === "Quizizz") {
+  var x = document.createElement("script");
+        x.src = "https://cdn.jsdelivr.net/gh/gbaranski/quizizz-cheat@master/dist/bundle.js";);
+        document.head.appendChild(x);
+}
